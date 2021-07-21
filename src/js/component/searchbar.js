611332d3c92d68c03f166232a56cf88e27634d07
amafjarkasi <amafjarkasi@gmail.com>
2021-07-21 13:10:02 +0000

    fix changes

--- src/js/component/searchbar.js
+++ src/js/component/searchbar.js
@@ -5,7 +5,7 @@ export default function SearchBar() {
 	const [searchTag, setSearchTag] = useState("");
 
 	function handleClick(e) {
-		console.log(e);
+		// console.log(e);
 	}
 
 	// function searchTag(e) {}
